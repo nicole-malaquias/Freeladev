2179e71957cc9f70b075e0c20acc4d8f7dad1f18
Julia Clepf <jumgc09@gmail.com>
2021-10-11 12:27:34 -0300

    feat: add password, email and cnpj verifiers

--- app/models/contractor_model.py
+++ app/models/contractor_model.py
@@ -1,7 +1,7 @@
 from app.configs.database import db
 from dataclasses import dataclass
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import re
 @dataclass
 class ContractorModel(db.Model):
     name: str
@@ -31,3 +31,23 @@ class ContractorModel(db.Model):
     def verify_password(self, password_to_compare):
         return check_password_hash(self.password_hash, password_to_compare)
 
+    @staticmethod
+    def verify_pattern_email(email):
+        pattern_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if(re.fullmatch(pattern_email, email)):
+            return True
+        else:
+            return False
+    @staticmethod
+    def verify_pattern_password(password):
+        pattern_password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+        if(re.search( pattern_password, password)):
+            return True
+        return False
+    @staticmethod
+    def verify_cnpj(cnpj):
+        pattern_cnpj = r'(^\d{2}.\d{3}.\d{3}/\d{4}-\d{2}$)'
+        if(re.fullmatch(pattern_cnpj, cnpj)):
+            return True
+        else:
+            return False
