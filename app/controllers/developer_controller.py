709f04f9eccd987fd9a0acd086a11979275f40b4
Nicole <nicolemalaquias91@gmail.com>
2021-10-11 12:41:20 -0300

    feature: make feature create developer

--- app/controllers/developer_controller.py
+++ app/controllers/developer_controller.py
@@ -1,9 +1,51 @@
-from flask_jwt_extended import (create_access_token, get_jwt_identity, jwt_required)
+from app.configs.database import db
+from app.exceptions.InvalidEmail import InvalidEmailError
+from app.exceptions.invalidPassword import InvalidPasswordError
+from app.exceptions.FieldCreateUserError import FieldCreateUserError
+from app.models.developer_model import DeveloperModel
+
+from flask import jsonify, request
+
+from http import HTTPStatus
+from flask_jwt_extended import (create_access_token, get_jwt_identity,
+                                jwt_required)
+
 
 def create_profile():
-    ...
+    
+    try :
+        
+        data = request.json
+        
+        verify_email = DeveloperModel.verify_pattern_email(data['email'])
+        if not verify_email:
+            raise InvalidEmailError(data)
 
+        verify_password = DeveloperModel.verify_pattern_password(data['password'])
+        if not verify_password:
+            raise InvalidPasswordError(data)
+        
+        password_input = data.pop('password')
+        
+        new_dev = DeveloperModel(**data)
+        new_dev.password = password_input
+        
+        db.session.add(new_dev)
+        db.session.commit()
 
+        return jsonify(new_dev),HTTPStatus.CREATED
+        
+    except InvalidEmailError as err:
+        return  jsonify(err.message)
+       
+    except InvalidPasswordError as err:
+        return jsonify(err.message)
+
+    except TypeError :
+        err = FieldCreateUserError()
+        return jsonify(err.message)
+    
+    
 @jwt_required()
 def get_profile_info():
     ...
@@ -15,8 +57,7 @@ def update_profile_info():
 @jwt_required()
 def delete_profile():
     ...
-
-
+    
 def get_all_developers():
     ...
 
---
+++ app/exceptions/FieldCreateUserError.py
@@ -0,0 +1,19 @@
+from flask import jsonify, request
+class FieldCreateUserError(Exception):
+
+    
+    def __init__(self):
+        data = request.json
+        data = list(data)
+        problem = [ i for i in data if i not in ["name","email","password","birthdate"] ]
+        
+        self.message = {
+            "Message": {
+                 "available_fields": [
+                "name","email","password","birthdate",
+                ],
+            "Wrong_keys_sended": [*problem]
+            }
+                     
+        }
+        super().__init__(self.message)
