15d6ed17a43f0fc355003ff86b4a0c27ce106c22
Lucira Silva <luciracs0@gmail.com>
2021-10-11 16:51:16 -0300

    feat: create more personalized exceptions and improve messages to errors

--- app/controllers/api_controller.py
+++ app/controllers/api_controller.py
@@ -1,16 +1,20 @@
-from app.exceptions.users_exceptions import UserNotFoundError
+from app.exceptions.users_exceptions import (InvalidPasswordError,
+                                             UserNotFoundError)
 from app.models.contractor_model import ContractorModel
 from app.models.developer_model import DeveloperModel
 from flask import jsonify, request
 from flask_jwt_extended import create_access_token
 
+
 def login():
     data = request.json
     
-    found_contractor = ContractorModel.query.filter_by(email=data['email']).first()
-    found_developer = DeveloperModel.query.filter_by(email=data['email']).first()
     
     try:
+        
+        found_contractor = ContractorModel.query.filter_by(email=data['email']).first()
+        found_developer = DeveloperModel.query.filter_by(email=data['email']).first()
+        
         if not found_contractor and not found_developer:
             raise UserNotFoundError
         
@@ -20,13 +24,25 @@ def login():
             if is_valid_password:
                 access_token = create_access_token(identity=found_developer)
                 return jsonify(access_token=access_token), 200
-        
+            
+            else:
+                raise InvalidPasswordError
+            
         elif found_contractor:
             is_valid_password = found_contractor.verify_password(data['password'])
             
             if is_valid_password:
                 access_token = create_access_token(identity=found_contractor)
                 return jsonify(access_token=access_token), 200
-
+            
+            else:
+                raise InvalidPasswordError
+            
     except UserNotFoundError as e:
         return {'message': str(e)}, 404
+    
+    except InvalidPasswordError as e:
+        return {'message': str(e)}, 406
+    
+    except KeyError as e:
+        return {'message': f'Is missing {str(e)} '}, 406
--- app/exceptions/users_exceptions.py
+++ app/exceptions/users_exceptions.py
@@ -1,6 +1,16 @@
 class UserNotFoundError(Exception):
     def __init__(self):
         
-        message = 'User not found. Make sure you send correct email and password'
+        message = 'User not found. Make sure you send the correct email!'
         
         super().__init__(message)
+        
+
+class InvalidPasswordError(Exception):
+    def __init__(self):
+        
+        message = 'Make sure you send the correct password!'
+        
+        super().__init__(message)
+        
+
