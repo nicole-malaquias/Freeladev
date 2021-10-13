40dfa5fd4a8b654375425f5af8c813f19f39f8c3
Julia Clepf <jumgc09@gmail.com>
2021-10-13 13:35:50 -0300

    feat: update job by id

--- app/controllers/job_controller.py
+++ app/controllers/job_controller.py
@@ -2,7 +2,7 @@ from flask_jwt_extended import (create_access_token, get_jwt_identity, jwt_requi
 from app.models.job_model import JobModel
 from app.models.contractor_model import ContractorModel
 from app.models.developer_model import DeveloperModel
-from flask import current_app, jsonify
+from flask import current_app, jsonify, request
 
 @jwt_required()
 def create_job():
@@ -17,7 +17,20 @@ def get_job_by_id(job_id: int):
 
 @jwt_required()
 def update_job_by_id(job_id: int):
-    ...
+    data = request.json
+    contractor = get_jwt_identity()
+    found_contractor = ContractorModel.query.filter_by(email=contractor["email"]).first()
+    job = JobModel.query.filter_by(id=job_id).first()
+    if job is None:
+        return {"message": "Job not found!"}, 404
+    if 'developer' in data:
+        developer = DeveloperModel.query.filter_by(email=data['developer']).first()
+        print(developer.id)
+    # if job.contractor_id == found_contractor.id:
+    #     job.update(data)
+    #     current_app.db.session.commit()
+    # check key errors
+    return "", 204
 
 @jwt_required()
 def delete_job_by_id(job_id: int):
