264954897c4ee6ba31c03c54a382a7fcd01e7f62
Julia Clepf <jumgc09@gmail.com>
2021-10-15 20:46:31 -0300

    fix: routes

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -1,6 +1,7 @@
 from . import contractor_blueprint, developer_blueprint, job_blueprint
 from flask import Blueprint
 from app.controllers.api_controller import login
+from app.controllers.job_controller import get_all_jobs
 
 bp = Blueprint('bp_api', __name__, url_prefix='/api')
 
@@ -8,3 +9,4 @@ bp.register_blueprint(contractor_blueprint.bp)
 bp.register_blueprint(developer_blueprint.bp)
 bp.register_blueprint(job_blueprint.bp)
 bp.post('/login')(login)
+bp.get('')(get_all_jobs)
